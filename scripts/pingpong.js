81aa741b41b80863cbc0c46cf167f299b9cfbc58
Ubuntu <ubuntu@ip-172-31-89-247.ec2.internal>
2019-06-09 15:57:09 +0000

    Refactor pingpong.js to check for strings other than Ping or Pong - test passes

--- scripts/pingpong.js
+++ scripts/pingpong.js
@@ -1,9 +1,14 @@
 pingPongGame = function(pingpong) {
-    if (pingpong === "Pong") {
-        return "Ping";
-    }
-    else if (pingpong === "Ping"){
-        return "Pong";
+    if(typeof pingpong == 'string') {
+        if (pingpong === "Pong") {
+            return "Ping";
+        }
+        else if (pingpong === "Ping") {
+            return "Pong";
+        }
+        else {
+            return "Please enter Ping, Pong or a number 0-10.";
+        }
     }
     else {
         return "Please enter Ping, Pong or a number 0-10.";
